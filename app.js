094715c3c9c5809e7410acb83d3eef7efc839e9e
Mohit sharma <mohitanand8987@gmail.com>
2024-08-23 00:16:10 +0530

    Routing and dividing the file of routing

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
 const http = require("http");
-const fs = require("fs");
-// function rqListener(req,res){
-
-// }
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title> Enter Message </title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></input></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY TEXT");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  // process.exit();
-  res.setHeader("content-type", "text/html");
-  res.write("<html>");
-  res.write("<head><title> First Page </title></head>");
-  res.write("<body><h1> Hello from my node.js server </h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+const server = http.createServer(routes);
 server.listen(3000);
